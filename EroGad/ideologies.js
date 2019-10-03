0101945f419326d6d8d1150ea22500154e2ce99f
MrKukok <40778356+MrKukok@users.noreply.github.com>
2019-10-04 00:30:04 +0200

    EroGad
    Dodano EroGad

---
+++ EroGad/ideologies.js
@@ -0,0 +1,28 @@
+ideologies = [
+    {
+        "name": "Gadulstwo",
+        "stats": {
+            "econ": 100,
+            "dipl": 0,
+
+        }
+    },
+    {
+        "name": "Centryzm Oświecony",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+
+        }
+    },
+    {
+        "name": "Trotskyism",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+
+        }
+    },
+    
+
+];
---
+++ EroGad/questions.js
@@ -0,0 +1,59 @@
+questions = [
+    {
+        "question": "Posiadam bogaty zasób słownictwa.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+        }
+    },
+    {
+        "question": "Słyszałem więcej niż trzy odcinki jakiegoś podcastu.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+        }
+    },
+    {
+        "question": "Boję się publicznych wystąpień",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+        }
+    },
+    {
+        "question": "Rzadko to ja rozpoczynam jakąkolwiek rozmowę.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+        }
+    },
+    {
+        "question": "Nie wstydzę się poruszania tematów okołoerotycznych.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+        }
+    },
+    {
+        "question": "Byłem w więcej niż dwóch związkach.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+        }
+    },
+    {
+        "question": "Jestem zadowolony ze sposobu, w jaki polskie szkolnictwo obecnie traktuje zajęcia wychowania do życia w rodzinie.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+        }
+    },
+    {
+        "question": "Nigdy nie widziałem żadnego filmu pornograficznego",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+        }
+    },
+ 
+];
